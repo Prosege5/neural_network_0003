d66e80e78f60a460a6bb13ee2ae6aa8f89c61cc6
Zlata-Salyukova <prosegelumpascoodle@gmail.com>
2024-01-03 20:40:52 -0600

    Day 7: neuron and layer function progress, now testing layer creation with outputs for testing

--- README.md
+++ README.md
@@ -25,3 +25,8 @@ Also, might start messing around with backward propagation with this program onc
 - Day 6: More program planning an laying out in /testing/layers program.
 Decided to make inputs a layer with an activation function of None. I will set 1 weight to 1 and bias to 0 to feed forward original input. Doing this method since I want to be able to save the layers in .json format and load from .json file.
 Backpropagation functions in site for learning more on how it works once I can start getting outputs from the layers.
+
+- Day 7: Progress has been made on inplementing functionality into neurons and layers.
+Today I wrote the code to create new neurons, set their random weights and bias.
+In the new layer method I check the layer type to setup an input layer out of neurons. I chose this method for when I setup layer loading and saving to and from .json format.
+Lots of code lines commented out to start testing initial outputs and if my code is functioning correctly. And ... it does! I create and input layer with a single weight of 1.0 and bias of 0.0 andn no activation function. This will just feed forward any future input as itself. The hidden layer and output layer are created and random weights and biases generated.
--- testing/layers/src/main.rs
+++ testing/layers/src/main.rs
@@ -3,29 +3,33 @@
 
 //* Crates */
 use rand::Rng;
-use std::error::Error;
-use std::fs;
-use std::fs::File;
-use std::io::Write;
-use csv::{Reader, Writer};
-use serde::{ Deserialize, Serialize };
-use serde_json;
+//use std::error::Error;
+//use std::fs;
+//use std::fs::File;
+//use std::io::Write;
+//use csv::{Reader, Writer};
+//use serde::{ Deserialize, Serialize };
+//use serde_json;
 
 //* Enums */
+#[derive(Debug)]
 // Activation function enum for Layer struct
-enum Activation_Function {
+enum ActivationFunction {
     Sigmoid,
-    ReLU,
+//    ReLU,
     Softmax,
     None,
 }
+
+#[derive(Debug)]
 // Layer type enum for Layer struct
-enum Layer_Type {
+enum LayerType {
     Input,
     Hidden,
     Output,
 }
 
+#[derive(Debug)]
 //* Structs and Methods */
 //Neuron struct and methods
 struct Neuron {
@@ -34,93 +38,127 @@ struct Neuron {
 }
 impl Neuron {
     //fn to create a new neuron
-    fn new_neuron() -> Neuron {
-
+    fn new_neuron(inputs: &Vec<f64>) -> Neuron {
+        Neuron {
+            weights: vec![1.0; inputs.len()],
+            bias: 0.0,
+        }
     }
     //generate random weights and bias
     fn gen_w_and_b(&mut self) {
-
+        let mut rng = rand::thread_rng();
+        let dec_places: u32 = 6;
+        for i in 0..self.weights.len() {
+            self.weights[i] = round_f64(rng.gen_range(-1.0..1.0), dec_places);
+        }
+        self.bias = round_f64(rng.gen_range(-5.0..5.0), dec_places);
     }
     //update weights and bias?? used with backward propagation??
-    fn update_w_and_b(&mut self) {
-
-    }
+//    fn update_w_and_b(&mut self) {
+//
+//    }
 }
+
+#[warn(dead_code)]
+#[derive(Debug)]
 //Layer struct and methods
 struct Layer {
-    type: Layer_Type,
+    layer_type: LayerType,
+    input_size: usize,
     layer_size: usize,
-    activation: Activation_Function,
+    activation: ActivationFunction,
     neurons: Vec<Neuron>,
 }
 impl Layer {
     //fn to create a new layer
-    fn new_layer(type: Layer_Type, layer_size: usize, activation: Activation_Function) -> Layer {
-
+    fn new_layer(layer_type: LayerType, input_size: usize, layer_size: usize, activation: ActivationFunction) -> Layer {
+        let neurons = match layer_type {
+            LayerType::Input => (0..layer_size).map(|_| Neuron::new_neuron(&vec![1.0])).collect(),
+            _ => (0..layer_size).map(|_| {
+                let mut neuron = Neuron::new_neuron(&vec![0.0; input_size]);
+                neuron.gen_w_and_b();
+                neuron
+            }).collect(),
+        };
+        
+        Layer {
+            layer_type,
+            input_size,
+            layer_size,
+            activation,
+            neurons,
+        }
     }
     // forward propagation
-    fn forward_propagation(&self, inputs: Vec<f64>) -> Vec<f64> {
-
-    }
-    // Sigmoid Function
-    fn sigmoid(output: f64) -> f64 {
-
-    }
-    // ReLU Function
-    fn relu(output: f64) -> f64 {
-
-    }
-    // Softmax Function
-    fn softmax(output: f64) -> f64 {
-
-    }
-    ////save model
-    fn save_layer() -> {
-
-    }
-    ////load model
-    fn load_layer() -> {
-
-    }
+//    fn forward_propagation(&self, inputs: Vec<f64>) -> Vec<f64> {
+//
+//    }
+//    // Sigmoid Function
+//    fn sigmoid(output: f64) -> f64 {
+//
+//    }
+//    // ReLU Function
+//    fn relu(output: f64) -> f64 {
+//
+//    }
+//    // Softmax Function
+//    fn softmax(output: f64) -> f64 {
+//
+//    }
+//    ////save model
+//    fn save_layer() -> {
+//
+//    }
+//    ////load model
+//    fn load_layer() -> {
+//
+//    }
 }
 
 //* Main Function */
 fn main() {
     //create inputs vec from csv file
-    let inputs: Vec<Vec<f64>> = load_inputs("inputs.csv", 3);
+//    let inputs: Vec<Vec<f64>> = load_inputs("inputs.csv", 3);
 
     //create a inputs layer
-    let mut input_layer: Layer = Layer::new_layer(Layer_Type::Input, 3, Activation_Function::None);
+    let input_layer: Layer = Layer::new_layer(LayerType::Input, 3, 3, ActivationFunction::None);
     //create a hidden layer
-    let mut hidden_layer: Layer = Layer::new_layer(Layer_Type::Hidden, 3, Activation_Function::Sigmoid);
+    let hidden_layer: Layer = Layer::new_layer(LayerType::Hidden, 3, 3, ActivationFunction::Sigmoid);
     //create a output layer
-    let mut output_layer: Layer = Layer::new_layer(Layer_Type::Output, 3, Activation_Function::Softmax);
+    let output_layer: Layer = Layer::new_layer(LayerType::Output, 3, 3, ActivationFunction::Softmax);
 
     //manually feed forward each layer to generate output
-    let output: Vec<f64> = output_layer.forward_propagation(
-        hidden_layer.forward_propagation(
-            input_layer.forward_propagation()
-        )
-    );
-
+//    let output: Vec<f64> = output_layer.forward_propagation(
+//        hidden_layer.forward_propagation(
+//            input_layer.forward_propagation()
+//        )
+//    );
+    println!("Input Layer: {:?}\n", input_layer);
+    println!("Hidden Layer: {:?}\n", hidden_layer);
+    println!("Output Layer: {:?}\n", output_layer);
 
 }
 
-//* Functions */
-fn load_inputs(file_name: &str) -> Vec<Vec<f64>> {
+//* Funct//ions */
+//fn load_inputs(file_name: &str) -> Vec<Vec<f64>> {
+//
+//}
 
+fn round_f64(value: f64, decimal_places: u32) -> f64 {
+    let factor = 10f64.powi(decimal_places as i32);
+    (value * factor).round() / factor
 }
 
 //* Training methods - Back propagation */
-    //Mean Squared Error (MSE) Loss
-    fn mse() -> {
-
-    }
-    //Partial Derivative
-    fn partial_derivative() -> {
-
-    }
-    //Stochastic Gradient Descent (SGD) 
-    fn sgd() -> {
-        
-    }
+//Mean Squared Error (MSE) Loss
+//fn mse() -> {
+//
+//}
+//Partial Derivative
+//fn partial_derivative() -> {
+//
+//}
+//Stochastic Gradient Descent (SGD) 
+//fn sgd() -> {
+//        
+//}
